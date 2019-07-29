dc440087dd00b7abc9d1497e4b52be5c3ff2d88e
TengshengHou <95377774@qq.com>
2019-07-29 13:16:39 +0800

    任务76： IdentityService4自定义Validator

---
+++ CoreService/User.Api/User.Identity/Authentication/SmsAuthCodeValidator.cs
@@ -0,0 +1,50 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using User.Identity.Services;
+
+namespace User.Identity.Authentication
+{
+    public class SmsAuthCodeValidator : IExtensionGrantValidator
+    {
+
+        IAuthCodeService _authCodeService;
+        IUserService _userService;
+        public SmsAuthCodeValidator(IAuthCodeService authCodeService, IUserService userService)
+        {
+            _authCodeService = authCodeService;
+            _userService = userService;
+        }
+
+        public string GrantType => "sms_auth_code";
+
+        public async Task ValidateAsync(ExtensionGrantValidationContext context)
+        {
+            var phone = context.Request.Raw["phone "];
+            var code = context.Request.Raw["auth_code"];
+            var errorValidationResult = new GrantValidationResult(TokenRequestErrors.InvalidGrant);
+            if (string.IsNullOrWhiteSpace(phone) || string.IsNullOrWhiteSpace(code))
+            {
+                context.Result = errorValidationResult;
+                return;
+            }
+            //检查验证码
+            if (!_authCodeService.Validate(phone, code))
+            {
+                context.Result = errorValidationResult;
+                return;
+            }
+            //完成用户注册
+            var userID = _userService.CheckOrCreate(phone);
+            if (userID <= 0)
+            {
+                context.Result = errorValidationResult;
+                return;
+            }
+            context.Result = new GrantValidationResult(userID.ToString(), GrantType);
+        }
+    }
+}
---
+++ CoreService/User.Api/User.Identity/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Identity.Services
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// 价差手机号码是否已经注册，如果没有注册的话就注册一个用户
+        /// </summary>
+        /// <param name="phone"></param>
+        int CheckOrCreate(string phone);
+    }
+}
