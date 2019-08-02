feff5c05a4b03d8040b3bb1a33a9069acc46a54c
TengshengHou <95377774@qq.com>
2019-08-02 13:18:12 +0800

    任务79：添加Ocelot网关并集成identity server4认证  14分钟

---
+++ CoreService/User.Api/API.Gateway/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Gateway.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((webHost, builder) =>
+            {
+                builder.SetBasePath(webHost.HostingEnvironment.ContentRootPath)
+                .AddJsonFile("Ocelot.json");
+            })
+            .UseStartup<Startup>()
+            .UseUrls("http://*:81");
+    }
+}
