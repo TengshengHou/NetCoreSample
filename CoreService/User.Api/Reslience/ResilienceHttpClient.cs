699846f13ca5c85b67a7cd0b3c923f8ca75fa1e4
[TengshengHou] <[95377774@qq.com]>
2019-08-18 11:42:59 +0800

    任务83：实现ResilienceHttpClient 完结 ：Polly记得用低版本，否则按照视频代码无法编译通过  可能有坑是视频中未给_policyCreator赋值，看下一课程视频吧

--- CoreService/User.Api/Reslience/ResilienceHttpClient.cs
+++ CoreService/User.Api/Reslience/ResilienceHttpClient.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Polly;
 using Polly.Wrap;
 
@@ -17,23 +19,80 @@ namespace Reslience
         //根据url origin 去创建policy
         private readonly Func<string, IEnumerable<Policy>> _policyCreator;
         //把去创建policy 打包组合 policy wraper 进行本地缓存
-        private readonly ConcurrentDictionary<string, PolicyWrap> _policyWraps;
+        private readonly ConcurrentDictionary<string, PolicyWrap> _policyWrappers;
         private ILogger _logger;
         private IHttpContextAccessor _httpContextAccessor;
         public ResilienceHttpClient(Func<string, IEnumerable<Policy>> policyCreator, ILogger<ResilienceHttpClient> logger, IHttpContextAccessor httpContextAccessor)
         {
+            _policyCreator = policyCreator;
             _httpClient = new HttpClient();
-            _policyWraps = new ConcurrentDictionary<string, PolicyWrap>();
+            _policyWrappers = new ConcurrentDictionary<string, PolicyWrap>();
             _logger = logger;
             _httpContextAccessor = httpContextAccessor;
         }
 
-        public Task<HttpResponseMessage> PostAsync<T>(string url, T item,
-            string authorizationToken, string requestId = null, string authorizationMethod = "Bearer")
+        public Task<HttpResponseMessage> PostAsync<T>(string url, T item, string authorizationToken, string requestId = null, string authorizationMethod = "Bearer")
         {
-            return null;
+            return DoPostAsync<T>(HttpMethod.Post, url, item, authorizationToken, requestId, authorizationMethod);
         }
 
-        public  
+
+        private async Task<HttpResponseMessage> DoPostAsync<T>(HttpMethod method, string url, T item, string authorizationToken, string requestId = null, string authorizationMethod = "Bearer")
+        {
+            if (method != HttpMethod.Post && method != HttpMethod.Put)
+            {
+                throw new ArgumentException("", nameof(method));
+            }
+            var origin = GetOriginFromUrl(url);
+            return await HttpInvoker(origin, async () =>
+            {
+                var requestMessage = new HttpRequestMessage(method, url);
+                SetAuthorizationHeader(requestMessage);
+                requestMessage.Content = new StringContent(JsonConvert.SerializeObject(item), System.Text.Encoding.UTF8, "application/json");
+                if (authorizationToken != null)
+                    requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(authorizationMethod, authorizationToken);
+                if (requestId != null)
+                    requestMessage.Headers.Add("x-requestid", requestId);
+                var response = await _httpClient.SendAsync(requestMessage);
+                if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                    throw new HttpRequestException();
+                return response;
+            });
+
+        }
+
+        private async Task<T> HttpInvoker<T>(string origin, Func<Task<T>> action)
+        {
+            var normalizedOrigin = NormalizedOrigin(origin);
+            if (!_policyWrappers.TryGetValue(normalizedOrigin, out PolicyWrap policyWrap))
+            {
+                var v = _policyCreator(normalizedOrigin).ToArray();
+                policyWrap = Policy.WrapAsync(v);
+                _policyWrappers.TryAdd(normalizedOrigin, policyWrap);
+            }
+            return await policyWrap.ExecuteAsync(action,
+                                                 new Context(normalizedOrigin));
+        }
+
+        private static string NormalizedOrigin(string origin)
+        {
+            return origin.Trim()?.ToLower();
+        }
+        private static string GetOriginFromUrl(string uri)
+        {
+            var url = new Uri(uri);
+            var origin = $"{url.Scheme}://{url.DnsSafeHost}:{url.Port}";
+            return origin;
+
+        }
+        private void SetAuthorizationHeader(HttpRequestMessage requestMessage)
+        {
+            var authorizationHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
+            if (!string.IsNullOrEmpty(authorizationHeader))
+            {
+                requestMessage.Headers.Add("Authorization", new List<string> { authorizationHeader });
+            }
+
+        }
     }
 }
