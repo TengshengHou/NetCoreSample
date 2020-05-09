4d08b0a99f2cac9f7d72281c5eac685b66ef6be8
TengshengHou <95377774@qq.com>
2020-05-09 10:33:34 +0800

    helloapi add HealthCheckController

---
+++ helloApi/helloApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DnsClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Recommend.API.Data;
+
+namespace helloApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+       
+        [HttpGet("")]
+        [HttpHead]
+        public ActionResult Ping()
+        {
+            return Ok();
+        }
+
+    }
+}
